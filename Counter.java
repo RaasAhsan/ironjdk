146fddd74e56de739bff260d902d8e7c7df27643
Raas Ahsan <raas.ahsan@gmail.com>
2019-03-25 20:50:11 -0700

    integer array instructions

--- Counter.java
+++ Counter.java
@@ -9,8 +9,12 @@ public class Counter {
     }
 
     public static void main(String[] args) {
-        int a = 0;
-        int b = 700;
+        int[] array = new int[] { 0, 1, 2, 3 };
+
+        array[0] = 3;
+
+        int a = array[0];
+        int b = -700;
         int c = 5;
         int d = a - b * c;
     }
--- src/runtime/interpreter.rs
+++ src/runtime/interpreter.rs
@@ -1,6 +1,8 @@
 use code::instruction::Instruction;
 use class::{ConstantPool, Method};
 use runtime::class::RuntimeMethod;
+use std::rc::Rc;
+use std::cell::RefCell;
 
 // TODO: Implement locals and stack with an array
 #[derive(Debug)]
@@ -19,8 +21,8 @@ impl StackFrame {
         self.stack.push(operand)
     }
 
-    fn get_local(&self, index: usize) -> StackValue {
-        self.locals[index]
+    fn get_local(&self, index: usize) -> &StackValue {
+        &self.locals[index]
     }
 
     fn set_local(&mut self, index: usize, var: StackValue) {
@@ -42,11 +44,20 @@ impl StackFrame {
         }
     }
 
+    fn pop_int_array(&mut self) -> Result<IntArray, InterpreterError> {
+        let operand = self.pop_stack().unwrap();
+
+        match operand {
+            StackValue::IntegerArrayReference(array) => Ok(array),
+            _ => Err(InterpreterError::UnexpectedOperand)
+        }
+    }
+
     fn get_int_local(&self, index: usize) -> Result<i32, InterpreterError> {
         let operand = self.get_local(index);
 
         match operand {
-            StackValue::Integer(i) => Ok(i),
+            StackValue::Integer(i) => Ok(*i),
             _ => Err(InterpreterError::UnexpectedOperand)
         }
     }
@@ -66,20 +77,45 @@ impl StackFrame {
 
 // A StackValue is any data type that can be stored in a variable.
 // In Java, there are two kinds of data types: primitive types and reference types.
-#[derive(Copy, Clone, Debug)]
+// Reference types are either objects or arrays.
+#[derive(Clone, Debug)]
 enum StackValue {
     Long(i64),
     Integer(i32),
     Short(i16),
     Byte(i8),
     Character(char),
+    IntegerArrayReference(IntArray),
     Empty
+}
+
+#[derive(Clone, Debug)]
+struct IntArray {
+    array: Rc<RefCell<Vec<i32>>>
+}
+
+impl IntArray {
+    fn get(&self, index: usize) -> i32 {
+        self.array.borrow()[index]
+    }
+
+    fn set(&mut self, index: usize, value: i32) {
+        self.array.borrow_mut()[index] = value;
+    }
+
+    fn new(size: usize) -> IntArray {
+        let array = Rc::new(RefCell::new(vec![0; size]));
+        IntArray {
+            array
+        }
+    }
 }
 
 #[derive(Debug)]
 enum InterpreterError {
     UnhandledInstruction(Instruction),
-    UnexpectedOperand
+    UnexpectedOperand,
+    InvalidArrayType
 }
 
 pub fn interpret(method: &RuntimeMethod, cp: &ConstantPool) {
@@ -99,17 +135,56 @@ pub fn interpret(method: &RuntimeMethod, cp: &ConstantPool) {
     }
 
     println!("{:?}", stack_frame);
+    println!("{:?}", std::mem::size_of::<Rc<RefCell<Vec<i32>>>>());
 }
 
 fn interpret_instruction(instruction: &Instruction, stack_frame: &mut StackFrame) -> Result<(), InterpreterError> {
     println!("{:?}", instruction);
 
     match instruction {
+        Instruction::Aload1 => {
+            let operand = stack_frame.get_local(1).clone();
+            stack_frame.push_stack(operand);
+            Ok(())
+        },
+        Instruction::Astore1 => {
+            let operand = stack_frame.pop_stack().unwrap();
+
+            stack_frame.set_local(1, operand);
+
+            Ok(())
+        },
+        Instruction::Dup => {
+            let operand = stack_frame.pop_stack().unwrap();
+
+            stack_frame.push_stack(operand.clone());
+            stack_frame.push_stack(operand.clone());
+
+            Ok(())
+        },
         Instruction::Iadd => {
             let value2 = stack_frame.pop_int()?;
             let value1 = stack_frame.pop_int()?;
 
             stack_frame.push_int(value1 + value2);
+
+            Ok(())
+        },
+        Instruction::Iaload => {
+            let index = stack_frame.pop_int()?;
+            let array = stack_frame.pop_int_array()?;
+            let value = array.get(index as usize);
+
+            stack_frame.push_int(value);
+
+            Ok(())
+        },
+        Instruction::Iastore => {
+            let value = stack_frame.pop_int()?;
+            let index = stack_frame.pop_int()?;
+            let mut array = stack_frame.pop_int_array()?;
+
+            array.set(index as usize, value);
 
             Ok(())
         },
@@ -143,6 +218,11 @@ fn interpret_instruction(instruction: &Instruction, stack_frame: &mut StackFrame
 
             stack_frame.push_int(value1 * value2);
 
+            Ok(())
+        },
+        Instruction::Iload { index } => {
+            let int = stack_frame.get_int_local(*index as usize)?;
+            stack_frame.push_int(int);
             Ok(())
         },
         Instruction::Iload0 => {
@@ -198,6 +278,19 @@ fn interpret_instruction(instruction: &Instruction, stack_frame: &mut StackFrame
 
             Ok(())
         },
+        Instruction::Newarray { atype } => {
+            let count = stack_frame.pop_int()?;
+            // These are array type codes. We could classify them.
+            match atype {
+                10 => {
+                    let array = StackValue::IntegerArrayReference(IntArray::new(count as usize));
+                    stack_frame.push_stack(array);
+
+                    Ok(())
+                },
+                _ => Err(InterpreterError::InvalidArrayType)
+            }
+        },
         Instruction::Sipush(value) => {
             stack_frame.push_int(*value);
             Ok(())
@@ -205,6 +298,6 @@ fn interpret_instruction(instruction: &Instruction, stack_frame: &mut StackFrame
         Instruction::Return => {
             Ok(())
         },
-        x => return Err(InterpreterError::UnhandledInstruction(*x))
+        x => Err(InterpreterError::UnhandledInstruction(*x))
     }
 }
